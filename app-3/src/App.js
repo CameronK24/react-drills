c88a7a2eabbd67edb714165fc1e088bd99366a99
CameronK24 <cskelly15@gmail.com>
2020-06-18 17:01:24 -0600

    finished app-3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      list: ['Luigi', 'Bambi', 'Beeter', "I'm", 'Your', 'Dad'],
+      filteredList: ['Luigi', 'Bambi', 'Beeter', "I'm", 'Your', 'Dad']
+    }
+  }
+
+  handleUserInput = (input) => {
+    this.setState({filteredList: this.state.list});
+    let filter = [];
+    for (let x = 0; x < this.state.list.length; x++) {
+      if (this.state.list[x].includes(input)) {
+        filter.push(this.state.list[x]);
+      }
+    }
+    // console.log(filter)
+    this.setState({filteredList: filter});
+  }
+
+  render() {
+    // this.state.filteredList = this.state.list;
+    // console.log(this.state.filteredList);
+    let formatList = this.state.filteredList.map((element, index) => {
+      return <h2 key={index}>{element}</h2>
+    })
+    return (
+      <div className="App">
+        <input onChange={(e) => this.handleUserInput(e.target.value)}/>
+        <h2>{formatList}</h2>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
